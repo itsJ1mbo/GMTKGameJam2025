9c7c90e8adb590dd2a11940ab0a4b3cc3fd2e344
David-104 <dcanelad@ucm.es>
2025-08-01 17:31:59 +0200

    rotaçao

--- Assets/Scripts/Rotable.cs
+++ Assets/Scripts/Rotable.cs
@@ -1,30 +1,39 @@
+using System;
 using UnityEngine;
 
 public class RotationComponent : MonoBehaviour
 {
     [SerializeField][Tooltip("Direccion de rotacion en sentido de las agujas del reloj")]
     private bool rotateClockwise = true;
+    [SerializeField]
+    private float rotationAngle = 90.0f;
     [SerializeField][Tooltip("Velocidad de rotacion")]
     private float rotationSpeed = 5.0f;
     [SerializeField][Tooltip("Eje de rotacion")]
     private Vector3 rotationAxis = Vector3.up;
-    
-    
-    //angulos que rota la cara
-    private float rotationAngle = 90.0f;
+    [SerializeField][Tooltip("Tamaño overlap")]
+    Vector3 halfExtents = Vector3.zero;
+    [SerializeField]
+    private GameObject centerRoom;
+
     private float amountRotated = 0.0f;
     private bool isRotating = false;
     private int rotationDirection;
-
-    void Start()
-    {
-        Rotate();
-    }
+    private Collider[] facesToRotate;
+    
+    public bool rotate = false;
+    
     void Update()
     {
+        if(rotate)
+            Rotate();
+        
         if (isRotating)
         {
-            transform.Rotate(rotationAxis, rotationDirection * rotationSpeed * Time.deltaTime);
+            foreach (Collider face in facesToRotate)
+            {
+                face.transform.RotateAround(centerRoom.transform.position, rotationAxis, rotationDirection * rotationSpeed * Time.deltaTime);
+            }
             amountRotated += rotationSpeed * Time.deltaTime;
             if (amountRotated >= rotationAngle)
             {
@@ -33,17 +42,19 @@ public class RotationComponent : MonoBehaviour
             }
         }   
     }
-    public void changeRotationDirection()
-    {
-        if(!isRotating)
-            rotateClockwise = !rotateClockwise;
-    }
     public void Rotate()
     {
         if (!isRotating)
         {
+            CheckCurrentFaces();
+            rotate = false;
             isRotating = true;
             rotationDirection = rotateClockwise ? 1 : -1;
         }
     }
+
+    void CheckCurrentFaces()
+    {
+        facesToRotate = Physics.OverlapBox(transform.position, halfExtents, Quaternion.identity, 64, QueryTriggerInteraction.Collide);
+    } 
 }
